ecdbad0f531e18a72ebf322fba065b69a0c09619
kennethxiong <kenneth.xiong@radrobotics.org>
2022-09-20 20:59:43 -0700

    implemented detection which will detect which color is being shown out of c,y,m

--- openCV-project/detectSignal.py
+++ openCV-project/detectSignal.py
@@ -17,23 +17,83 @@ ap.add_argument("-c", "--camera", required = False, default = None,
                 help = "integer representing the camera(if only one device, 0)")
 args = vars(ap.parse_args())
 
+def createMask(img, thresholdLB, thresholdUB):
+    """
+    Purpose: Applies preprocessing(blur/morph ops) and creates a mask for
+    contour drawing.
+    Parameters: image frame, hsv threshold(np.array()) lower bound and upper bound
+    to apply.
+    Return Val: a black and white image mask
+    """
+    #image preprocessing
+    blurredFrame = cv.GaussianBlur(img, (33,33), 0)
+    #thresholding
+    hsvFrame = cv.cvtColor(blurredFrame, cv.COLOR_BGR2HSV)
+    threshold = cv.inRange(hsvFrame, thresholdLB, thresholdUB)
+    #morphology ops
+    kernel1 = cv.getStructuringElement(cv.MORPH_ELLIPSE, (23,23))
+    kernel2 = np.ones((15,15), np.uint8)
+    #eliplical erode to get rid of some of the noise
+    erodedFrame = cv.erode(threshold, kernel1, iterations = 1)
+    openFrame = cv.dilate(erodedFrame, kernel2, iterations = 1)
+
+    return openFrame
+
+def findLargestContour(mask1, mask2, mask3):
+    """
+    Purpose: finds the mask that has the largest contour. This should mean
+            that the color the mask represents is the one being shown.
+    Parameters: 3 different masks
+    Return Val: the mask that has the largest contour
+    """
+    #detect contours
+    (cnts1, _) = cv.findContours(mask1.copy(),cv.RETR_LIST,cv.CHAIN_APPROX_SIMPLE)
+    (cnts2, _) = cv.findContours(mask2.copy(),cv.RETR_LIST,cv.CHAIN_APPROX_SIMPLE)
+    (cnts3, _) = cv.findContours(mask3.copy(),cv.RETR_LIST,cv.CHAIN_APPROX_SIMPLE)
+    cnts = [cnts1, cnts2, cnts3]
+    #find the max contour
+    maxC = np.array([])
+    maxMask = None
+    for c in cnts:
+        if len(c) != 0 :
+            if maxC.size == 0:
+                maxC = max(c, key=cv.contourArea)
+                maxMask = c
+            else:
+                if cv.contourArea(max(c, key=cv.contourArea)) > cv.contourArea(maxC):
+                    maxC = max(c, key=cv.contourArea)
+                    maxMask = c
+
+    #find the largest mask
+    if maxC in cnts1:
+        return mask1, maxC
+    if maxC in cnts2:
+        return mask2, maxC
+    else:
+        return mask3, maxC
+
 def main():
+    #check which input was used
     if args["camera"] != None:
         vid = cv.VideoCapture(int(args["camera"]))
     elif args["video"] != None:
         vid = cv.VideoCapture(args["video"])
     elif args["image"] != None:
         vid = cv.imread(args["image"])
+
     #hsv values for thresholding
-    low_H = 125
-    low_S = 55
-    low_V = 124
-    high_H = 150
-    high_S = 255
-    high_V= 255
+    cyanHSV_LB = np.array([75,50,0])
+    cyanHSV_UB = np.array([110,255,255])
+
+    yellowHSV_LB = np.array([10,50,0])
+    yellowHSV_UB = np.array([30,255,255])
+
+    magentaHSV_LB = np.array([150,50,0])
+    magentaHSV_UB = np.array([175,255,255])
+
     # display video/webcam feed
     while True:
-        #need to flip video
+        #check if video needs to be flipped
         if args["image"] != None:
             frame = vid
             ret, frame = vid.read()
@@ -43,48 +103,23 @@ def main():
             if args["video"] != None:
                 frame = cv.flip(frame, 0)
 
+        #get masks for each option/color
+        cyanMask = createMask(frame, cyanHSV_LB, cyanHSV_UB)
+        yellowMask = createMask(frame, yellowHSV_LB, yellowHSV_UB)
+        magentaMask = createMask(frame, magentaHSV_LB, magentaHSV_UB)
+        solutionMask = findLargestContour(cyanMask, yellowMask, magentaMask)
+        if solutionMask[1].size > 0:
+            (x,y,w,h) = cv.boundingRect(solutionMask[1])
+            cv.rectangle(frame, (x, y), (x+w,y+h), (0,255,0), 2)
 
-        image = frame.copy()
-        #image preprocessing
-        frame = cv.GaussianBlur(frame, (33,33), 0)
-        hsvFrame = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
-        threshold = cv.inRange(hsvFrame, (low_H, low_S, low_V), (high_H, high_S, high_V))
-        #morphology?
-        kernel1 = cv.getStructuringElement(cv.MORPH_ELLIPSE, (23,23))
-        kernel2 = np.ones((15,15), np.uint8)
-        eroded = cv.erode(threshold, kernel1, iterations = 1)
-        open = cv.dilate(threshold, kernel2, iterations = 1)
-        # dilate = cv.dilate(threshold, (20,20), iterations = 1)
-        # kernel = np.ones((5,5),np.uint8)
-        # open = cv.erode(threshold, kernal, iterations = 1)
-        # open = cv.morphologyEx(threshold, cv.MORPH_CLOSE, (10,10))
-        # open = cv.GaussianBlur(open, (17,17), 0)
-        # erod =
-        # closing = cv.morphologyEx(open, cv.MORPH_CLOSE, (20,20))
-        # eroded = cv.erode(closing, (25,25), iterations = 1)
-        # open = cv.morphologyEx(eroded, cv.MORPH_OPEN, (7,7))
-        (cnts, _) = cv.findContours(open.copy(),cv.RETR_LIST,cv.CHAIN_APPROX_SIMPLE)
-        if len(cnts) > 0:
-            # c = max(cnt, key=cv.)
-            for c in cnts:
-                (x,y,w,h) = cv.boundingRect(c)
-            # mask = np.zeros(image.shape[:2], dtype='uint8')
-                cv.rectangle(image, (x, y), (x+w, y+h), (0,255,0), 3)
-        # cv.rectangle(mask, (x, y), (x+w, y+h), (0,255,0), 2)
-        # cone = cv.bitwise_and(image, image, mask=mask)
+        if np.array_equal(solutionMask[0], cyanMask):
+            print("pos 3")
+        elif np.array_equal(solutionMask[0], yellowMask):
+            print("pos 1")
+        else:
+            print("pos2")
 
-        # cv.drawContours(image,[box],0,(0,0,255),2)
-        # for (i,c) in enumerate(cnts):
-        #     (x,y,w,h) = cv.boundingRect(c)
-        #     rectangle = cv.rectangle(image, (x, y), (x+w, y+h), (0,255,0), 2)
-        # cv.drawContours(image, cnts, -1, (0,255,0), 2)
-        # cv.imshow("open", open)
-        # cv.imshow("Cone", cone)
-        cv.imshow("Image", image)
-        # cv.imshow("frame", frame)
-        cv.imshow("mask", open)
-        # cv.imshow("hsv ", hsvFrame)
-        #click q to stop
+        cv.imshow("Image", frame)
         if cv.waitKey(1) == ord('q'):
             break
 
